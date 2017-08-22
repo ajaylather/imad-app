a615f4e66a812a7f926c7b853bb168a6a80271c6
ajaylather <ajaylather2050@gmail.com>
2017-08-22 09:56:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,8 +82,8 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var articleName=req.params.articleName;
 app.get('/:articleName', function(req, res){
+    var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName])); 
 });
 
